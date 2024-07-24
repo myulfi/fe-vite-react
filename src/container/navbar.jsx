124eb231886004002b199e86ecba2fca205e5dec
Mulyadi Yulfi <myulfi@xl.co.id>
2024-07-24 14:46:54 +0700

    Change a to Link for access Home

--- src/container/navbar.jsx
+++ src/container/navbar.jsx
@@ -1,5 +1,6 @@
 import NavbarLink from "./navbarLink";
 import './navbar.css';
+import { Link } from "react-router-dom";
 
 export default function Navbar({
     data
@@ -7,7 +8,7 @@ export default function Navbar({
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm border border-top-0">
             <div className="container">
-                <a className="navbar-brand" href="/"><span className="bi-umbrella">&nbsp;FE-VITE</span></a>
+                <Link to="/" className="navbar-brand"><span className="bi-umbrella">&nbsp;FE-VITE</span></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>

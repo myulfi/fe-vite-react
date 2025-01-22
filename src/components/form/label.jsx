3e85600810660f70447f545ca5a75e76fb0ea0f6
Mulyadi Yulfi <myulfi@xl.co.id>
2025-01-22 14:52:19 +0700

    Fixing onCopy in label.jsx and table.jsx

--- src/components/form/label.jsx
+++ src/components/form/label.jsx
@@ -4,7 +4,39 @@ export default function Label({
     copy = false,
     className,
 }) {
-    const onCopy = value => navigator.clipboard.writeText(value)
+    const onCopy = (e, value) => {
+        const div = document.createElement("div")
+        div.innerHTML = value
+        document.body.appendChild(div)
+        const range = document.createRange()
+        range.selectNode(div)
+        window.getSelection().removeAllRanges()
+        window.getSelection().addRange(range)
+        document.execCommand("copy")
+        window.getSelection().removeAllRanges()
+        document.body.removeChild(div)
+
+        const tooltip = document.createElement("div")
+        tooltip.innerHTML = `${value} copied`
+        tooltip.style.opacity = 0.8;
+        tooltip.style.left = `${e.pageX + 10}px`
+        tooltip.style.top = `${e.pageY + 10}px`
+        tooltip.style.position = "absolute"
+        tooltip.style.backgroundColor = "#333"
+        tooltip.style.color = "#FFF"
+        tooltip.style.padding = "7px"
+        tooltip.style.borderRadius = "5px"
+        tooltip.style.fontSize = "12px"
+        tooltip.style.transition = "opacity 0.3s"
+        tooltip.style.pointerEvents = "none"
+        tooltip.style.zIndex = 99999
+        document.body.appendChild(tooltip)
+
+        setTimeout(() => {
+            document.body.removeChild(tooltip)
+        }, 1000)
+    }
+
     return (
         <div className={`form-group mb-3 ${className}`}>
             <label className="form-label fw-bold">{text}</label>
@@ -13,7 +45,7 @@ export default function Label({
                 {
                     copy
                     && <>
-                        &nbsp;<i className="pl-1 bi-copy" role="button" onClick={() => onCopy(value)} />
+                        &nbsp;<i className="pl-1 bi-copy" role="button" onClick={(e) => onCopy(e, value)} />
                     </>
                 }
             </label>
--- src/components/table.jsx
+++ src/components/table.jsx
@@ -96,7 +96,38 @@ export default function Table({
         setDetailRow(itemArray.map(() => false))
     }, [resetPagination])
 
-    const onCopy = value => navigator.clipboard.writeText(value)
+    const onCopy = (e, value) => {
+        const div = document.createElement("div")
+        div.innerHTML = value
+        document.body.appendChild(div)
+        const range = document.createRange()
+        range.selectNode(div)
+        window.getSelection().removeAllRanges()
+        window.getSelection().addRange(range)
+        document.execCommand("copy")
+        window.getSelection().removeAllRanges()
+        document.body.removeChild(div)
+
+        const tooltip = document.createElement("div")
+        tooltip.innerHTML = `${value} copied`
+        tooltip.style.opacity = 0.8;
+        tooltip.style.left = `${e.pageX + 10}px`
+        tooltip.style.top = `${e.pageY + 10}px`
+        tooltip.style.position = "absolute"
+        tooltip.style.backgroundColor = "#333"
+        tooltip.style.color = "#FFF"
+        tooltip.style.padding = "7px"
+        tooltip.style.borderRadius = "5px"
+        tooltip.style.fontSize = "12px"
+        tooltip.style.transition = "opacity 0.3s"
+        tooltip.style.pointerEvents = "none"
+        tooltip.style.zIndex = 99999
+        document.body.appendChild(tooltip)
+
+        setTimeout(() => {
+            document.body.removeChild(tooltip)
+        }, 1000)
+    }
 
     const onPageChange = (page, length, search) => {
         if (CommonConstants.TABLE.LOAD_MORE === type && page === 1) {
@@ -357,7 +388,8 @@ export default function Table({
                                                                     &nbsp;<i
                                                                         className="pl-1 bi-copy"
                                                                         role="button"
-                                                                        onClick={() => onCopy(
+                                                                        onClick={(e) => onCopy(
+                                                                            e,
                                                                             column.render != undefined
                                                                                 ? column.render(getNestedValue(data, column.data), data)
                                                                                 : getNestedValue(data, column.data) ?? (column.defaultContent ? column.defaultContent() : "")
@@ -389,7 +421,8 @@ export default function Table({
                                                                             &nbsp;<i
                                                                                 className="pl-1 bi-copy"
                                                                                 role="button"
-                                                                                onClick={() => onCopy(
+                                                                                onClick={(e) => onCopy(
+                                                                                    e,
                                                                                     column.render != undefined
                                                                                         ? column.render(getNestedValue(data, column.data), data)
                                                                                         : getNestedValue(data, column.data) ?? (column.defaultContent ? column.defaultContent() : "")
